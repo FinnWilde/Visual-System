df258768b807ed7baa301e503e9a94e424a36654
Finlay Wilde <finlaywilde@gmail.com>
2025-06-19 13:19:03 +0100

    Updated to use class

--- 0.1.0/main.cpp
+++ 0.1.0/main.cpp
@@ -6,166 +6,52 @@
 
 using namespace Eigen;
 
-Vector2d ode(double c, double k, double m, double x, double v)
-{
+class HarmonicOscillator {
+public:
+    HarmonicOscillator(double mass, double damping, double stiffness)
+        : m(mass), c(damping), k(stiffness) {}
 
-    double dxdt = v;
-    double dvdt = (-(c * v) - (k * x)) / m;
-    
-    return Vector2d(dxdt, dvdt);
-
-}
-
-Vector2d rk4(double h, double x, double v, double c, double k, double m)
-{   
-    Vector2d prev(2);
-    prev(0) = x;
-    prev(1) = v;
-       
-    Vector2d k1 = ode(c, k, m, x, v);
-    Vector2d k2 = ode(c, k, m, x + h * k1[0] / 2, v + h * k1[1] / 2);
-    Vector2d k3 = ode(c, k, m, x + h * k2[0] / 2, v + h * k2[1] / 2);
-    Vector2d k4 = ode(c, k, m, x + h * k3[0], v + h * k3[1]);
-
-    return  prev + (h / 6) * (k1 + 2 * k2 + 2 * k3 + k4);
-
-}
-
-int main() {
-
-    double t = 0;
-    double m = 2;
-    double c = 5;
-    double k = 3;
-    double h = 0.1;
-    double x0 = 1;
-    double v0 = 0;
-
-    t = t + h;
-    Vector2d next = rk4(h, x0, v0, c, k, m);
-    
-    std::cout <<"Time (t): " << t
-        << ", Position (x): " << next[0]
-        << ", Velocity (v): " << next[1] << std::endl;
-
-    for (int i = 0; i < 200; i++) {
-
-        t = t + h;
-        double x = next[0];
-        double v = next[1];
-
-        next = rk4(h, x, v, c, k, m);
-        
-        std::cout << "Time (t): " << t
-            << ", Position (x): " << next[0]
-            << ", Velocity (v): " << next[1] << std::endl;
+    Vector2d ode(const Vector2d& state) const {
+        double x = state[0];
+        double v = state[1];
+        return Vector2d(v, (-c * v - k * x) / m); //row 0 = dx/dt, row 1 = dv/dt
     }
 
-  
+    Vector2d rk4(double h, const Vector2d& state) const {
+        Vector2d k1 = ode(state);
+        Vector2d k2 = ode(state + (h / 2) * k1);
+        Vector2d k3 = ode(state + (h / 2) * k2);
+        Vector2d k4 = ode(state + h * k3);
+        return state + (h / 6) * (k1 + 2 * k2 + 2 * k3 + k4);
+    }
+
+private:
+    double m; // Mass
+    double c; // Damping coefficient
+    double k; // Stiffness
+};
+
+int main() {
+    double t = 0;
+    double h = 0.01;
+    Vector2d state(2, 0); // Initial state: [x0, v0]
+
+    // Initialize oscillator with constants
+    HarmonicOscillator oscillator(2.0, 0, 3.0);
+
+    // First step
+    t += h;
+    state = oscillator.rk4(h, state);
+    std::cout << "Time (t): " << t << ", Position (x): " << state[0]
+        << ", Velocity (v): " << state[1] << std::endl;
+
+    // Subsequent steps
+    for (int i = 0; i < 2000; i++) {
+        t += h;
+        state = oscillator.rk4(h, state);
+        std::cout << "Time (t): " << t << ", Position (x): " << state[0]
+            << ", Velocity (v): " << state[1] << std::endl;
+    }
+
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
